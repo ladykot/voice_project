c6ec54eba3a2297eb36787497f23590146fd0a2c
Katya Akilina <katyaartline@gmail.com>
2019-07-03 13:34:46 +0300

    добавлены функции генерации, обновления и считывания токенов из конфига

--- main.py
+++ main.py
@@ -1,38 +1,67 @@
-import requests
-import time
 import json
-
-import pyglet
-import speech_recognition as sr
+import configparser
+import os
+import requests
 import subprocess
+import time
+
+import simpleaudio as sa
+import speech_recognition as sr
 
 
-IAM_TOKEN = "CggaATEVAgAAABKABJJx5U9Z0-5jdng2gH8Z3-IK_4CCGUzDSv6MPT7fuGs9wwWdACF6gpSaPIzJ7_qTLHobF2dABEZx8evRci7Y-ilZBfrWZe9Tsa1EO7vEYCDLD1Fb2Yt8ZshDAC1XliDLrRDixT6wmka4ObKChqcmyi51IbWXoGowHAvBZAXMWO3gk_L-leiKsYNUMNPOgZQ4uq8Ld8SGJu3bBXs88-xw2eYVlHKP_MdmWa4AW6oVN4pHnDPvGZgSs101qXaG3rFYomU30GDGzlxqJFSmWFCjA-ZTHlLoTCLFCWryIZX94jx_P1PwWTcxRZgmdDAWAC7qOpiMtkWaeTh7Ud7ymUQDUikhYHoaqiS51H8WufuH-qRAWrZ2YvvlRT8yxMyo3CdWGCGj1ZNukoav6D62I1gfjmFZI3w_OF6ZuvwTzwcuHmCA0yuMX5CaJZzw_MM6io-39QE8g1mvKZ09tkL5BWNVbOwzg_r5m9LdXE8vjdfCbafecTp1tdZ2u6cR8ikZgbfUAqizV4E2MfcietqYGbpuHmS-jFZfJNxPhDWY9DLAiphRYN5vIoT3OP9SnLX7S0UW-azLgDfiuZPKXhzKB8ZrRQ5GcdpiDsnV2Nv-BgMyYA7NcQWj0zwzRyRiAaH9xxXRZIKoE3XM6iXuvhtka7t6VnoqbimyiiaKl6whJRyJ2bWIGmEKIDNjZDVkZDdlYWVlNTQ3ZDI4MTQ4MmVlNjVjNjIyNWVkEPWS3ugFGLXk4OgFIh8KFGFqZTJuYmdpdjYxYTB1YmJnMTE0EgdsYWR5a290WgAwAjgBSggaATEVAgAAAFABIPAE"
-FOLDER_ID = "b1gb67gpn957qc6ctltf"
 URL = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
 
 
 replies = {
-    "привет": "hello, patient, do you have any pain?",
-    "да": "the doctor will bring medicine",
-    "я все": "good bye, bless you"
+    "привет": "здравствуйте, пациент, у вас что-то болит?",
+    "да": "доктор принес+ёт лекарства",
+    "я все": "до связи, будьте здоровы"
 }
 
 
-# функция включения звукового сигнала
-def signal():
-    music = pyglet.resource.media('Harp 1.wav')
-    music.play()
+def create_config(path):  # создание файла settings.ini, если его не существует
+    config = configparser.ConfigParser()
+    config.add_section("Settings")
+    config.set("Settings", "iam_token", "iam_token")
+    with open(path, "w") as config_file:   # записываем изменения
+        config.write(config_file)
 
 
-def recognize(data_sound):
+def read_id_folder(path):  # функция считывает id_folder из конфига
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config.items("Settings", 'id_folder')[1][1]
+
+
+def crud_config(path):  # функция генерирует, записывает в settings.ini и возвращает новый токен
+    if not os.path.exists(path):  # если конфиг-файла нет, создаем
+        create_config(path)
+
+    config = configparser.ConfigParser()  # создаем конфиг-парсер
+    config.read(path)  # просим его прочесть конфиг
+    result = subprocess.run("yc iam create-token".split(" "),
+                            stdout=subprocess.PIPE).stdout.decode('utf-8')[:-1]  # запуск генерации токена
+    config.set("Settings", "iam_token", result)  # меняем значение токена на новое сгенерированное
+    with open(path, "w") as config_file:   # записываем изменения в settings.ini
+        config.write(config_file)
+
+    return config.get("Settings", "iam_token")
+
+
+def signal():  # функция включения звукового сигнала
+    wave_obj = sa.WaveObject.from_wave_file("Harp 1.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+
+def recognize(data_sound):  # функция распознавания русской речи
     headers = {
-        'Authorization': 'Bearer ' + IAM_TOKEN,
+        'Authorization': 'Bearer ' + iam_token,  # добваляем новый токен в headers
     }
 
     params = {
         'lang': 'ru-RU',
-        'folderId': FOLDER_ID,
+        'folderId': id_folder,
         'sampleRateHertz': 48000,
     }
 
@@ -49,22 +78,24 @@ def main():
     recognizer = sr.Recognizer()
     while True:
         with sr.Microphone() as source:
-            #  subprocess.call(["say", "say something"])  # быстро надоест это слушать
-            recognizer.adjust_for_ambient_noise(source)  # настройка микрофона
+            recognizer.adjust_for_ambient_noise(source, duration=0.5)  # настройка микрофона
             data_sound = recognizer.listen(source, phrase_time_limit=7)  # пациент что-то отвечает ..> data_sound
             data_sound = data_sound.get_flac_data()
 
         recognize_text = recognize(data_sound)  # ответ пациента распознается через Yandex Api, получаем в результате текст
 
-        subprocess.call(["say", replies.get(recognize_text.lower())])
+        subprocess.call(["say", "-v", "Milena", replies.get(recognize_text.lower())])
         if recognize_text == "я все":
             signal()
-            time.sleep(5)  # необходимое время для выполнения signal()
+            time.sleep(1.7)  # необходимое время для выполнения signal()
             break
 
 
 if __name__ == "__main__":
-    subprocess.call(["say", "welcome to voice helper"])  # Приветствие
-    subprocess.call(["say", "speak after the signal"])
+    path = "settings.ini"  # определяем путь к конфигу
+    iam_token = crud_config(path)  # генерируем новый токен
+    id_folder = read_id_folder(path)  # считывваем id_folder с конфига
+    subprocess.call(["say", "-v", "Milena", "добро пожаловать, я ваш голосовой помощник"])  # Приветствие
+    subprocess.call(["say", "-v", "Milena", "говорите после сигнала"])
     signal()
     main()
