c6e1e20e0559c20df1d2d6680390002a1572952b
Katya Akilina <katyaartline@gmail.com>
2019-07-06 00:38:30 +0300

    токены импортируются явно, код изменен по pep8

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import json
 import requests
 import subprocess
 
-import settings
 import simpleaudio as sa
 import speech_recognition as sr
 
-
-IAM_TOKEN = settings.IAM_TOKEN
-ID_FOLDER = settings.ID_FOLDER
+from settings import ID_FOLDER, IAM_TOKEN, URL
 
 
 replies = {
@@ -18,10 +15,9 @@ replies = {
 }
 
 
-
 def recognize(data_sound):  # функция распознавания русской речи
     headers = {
-        'Authorization': 'Bearer ' + IAM_TOKEN,  # добваляем новый токен в headers
+        'Authorization': 'Bearer ' + IAM_TOKEN,  # новый токен >> headers
     }
 
     params = {
@@ -30,11 +26,12 @@ def recognize(data_sound):  # функция распознавания русс
         'sampleRateHertz': 48000,
     }
 
-    resp = requests.post(settings.URL, params=params, headers=headers, data=data_sound)  # отправка post-запроса
+    resp = requests.post(URL, params=params,
+                         headers=headers, data=data_sound)  # отправка post-запроса
     decode_resp = resp.content.decode('UTF-8')  # декодируем
     text = json.loads(decode_resp)  # загружаем в json
     if text.get("error_code") is None:
-        text = text.get("result")  # забираем текст из json по ключу result
+        text = text.get("result")  # текст << json по ключу result
         print(text)
     else:
         raise requests.RequestException('Ошибка на сервере')
@@ -51,18 +48,23 @@ def main():
     recognizer = sr.Recognizer()
     while True:
         with sr.Microphone() as source:
-            recognizer.adjust_for_ambient_noise(source, duration=0.3)  # настройка микрофона
-            data_sound = recognizer.listen(source, phrase_time_limit=7)  # пациент что-то отвечает ..> data_sound
+            recognizer.adjust_for_ambient_noise(source,
+                                                duration=0.3)  # настройка микрофона
+            data_sound = recognizer.listen(source,
+                                           phrase_time_limit=7)  # ответ от пациента
             data_sound = data_sound.get_flac_data()
-        recognize_text = recognize(data_sound)  # ответ пациента распознается через Yandex Api, получаем в результате текст
-        subprocess.call(["say", "-v", "Milena", replies.get(recognize_text.lower())])
+        recognize_text = recognize(data_sound)  # распознавание ответа
+        subprocess.call(["say", "-v", "Milena",
+                         replies.get(recognize_text.lower())])
         if recognize_text == "я все":
             signal()
             break
 
 
 if __name__ == "__main__":
-    subprocess.call("say -v Milena добро пожаловать! Я ваш робот-медсестра".split(" "))  # Приветствие
-    subprocess.call("say -v Milena чтобы я вам помогла, скажите после сигнала слово привет".split(" "))  # нужно один раз для нового пациента
+    subprocess.call("say -v Milena добро пожаловать! "
+                    "Я ваш робот-медсестра".split(" "))  # Приветствие
+    subprocess.call("say -v Milena чтобы я вам помогла, "
+                    "скажите после сигнала слово привет".split(" "))  # 1 раз при знакомстве
     signal()
     main()
