43d44ad8ae448801d3879c9b6e2b15afa26c1f6c
Katya Akilina <katyaartline@gmail.com>
2019-08-02 11:06:54 +0300

    Реализовано чтение из БД задач от пациента

---
+++ webapp/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from webapp.model import db, Tasks
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        title = 'Список дел от Милены'
+        tasks_milena = Tasks.query.with_entities(Tasks.task).all()
+
+        return render_template("index.html", page_title=title, tasks_milena_list=tasks_milena)
+
+    return app
+
